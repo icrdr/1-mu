845c58642c7afe175b1aec638490accc2067d8b1
icrdr <icrdr2010@gmail.com>
2019-07-08 03:16:16 +0800

    1

--- app/restful/wechat.py
+++ app/restful/wechat.py
@@ -66,7 +66,8 @@ class WxApi(Resource):
         }
 
         # 将字典转换为xml字符串
-        resp_xml_str = xmltodict.unparse(resp_dict, encoding='GBK')
+        resp_xml_str = xmltodict.unparse(resp_dict, encoding='utf-8')
+        print(resp_xml_str)
         # 返回消息数据给微信服务器
         return resp_xml_str, 200
             
